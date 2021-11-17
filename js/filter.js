534806af91e3691485a81a270d119193d39a7929
natali <rondam@yandex.ru>
2021-11-17 19:25:55 +0200

    правки по eslint

--- js/filter.js
+++ js/filter.js
@@ -2,8 +2,6 @@ const filters  = new Map([]);
 export const setupFiltersAsync = (action)=>new Promise((onSuccess)=>{
   const filtersForm = document.querySelector('.map__filters');
   filtersForm.addEventListener('change',(event)=>{
-    // eslint-disable-next-line no-console
-    console.log(event.target.type);
     const filterName = event.target.name.replace('housing-','');
     const filterValue = event.target.value;
     if(event.target.type!=='checkbox'){
@@ -21,8 +19,6 @@ export const setupFiltersAsync = (action)=>new Promise((onSuccess)=>{
         }
       }
     }
-    // eslint-disable-next-line no-console
-    console.log(filters);
     action();
     onSuccess();
   });
